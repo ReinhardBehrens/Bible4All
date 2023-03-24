67eb1b930f355898bef0378ad96c112a1dbe9b13
ReinhardBehrens <124669682+ReinhardBehrens@users.noreply.github.com>
2023-03-24 12:29:07 +0200

    DatabaseConnection Updated, slow progress

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -2,34 +2,35 @@
 
 class DatabaseConnection {
 
-            public $connectionInfo;
-            public $conn;
-            public $serverName;
-            
-            function __construct($connectionInfo, $connection, $serverName)
-            {
-                $this->$connectionInfo=$connectionInfo;
-                $this->$conn = $connection;
-                if($connection) {
-                     //echo "Connection established.<br />";
-                }else{  
-                     echo "Connection could not be established.<br />";
-                     die(print_r(sqlsrv_errors(), true));
-                }
-                
-                $this->$serverName=$serverName;
-            }
-            
-            
-            public get_connection() 
-            {
-                return $this->$conn;
-            }
-            
-//            $serverName = "REIN\\SQLEXPRESS"; //serverName\instanceName
-//            $connectionInfo = array( "Database"=>"BibleForAllServerAppv1.Server.Data", "UID"=>"sa", "PWD"=>"password", "CharacterSet" =>"UTF-8");
-//            $conn = sqlsrv_connect( $serverName, $connectionInfo);
+    public $connectionInfo;
+    public $connection;
+    public $serverName;
 
-            
-          
+    function __construct(array $connectionInfo, $serverName)
+    {
+        $connectionInfo=array();
+        
+        foreach($connectionInfo as $connectData)
+        {
+            $this->connectionInfo[] = $connectData;
+        }
+        
+        $this->$serverName=$serverName;
+
+        $this->$connection = sqlsrv_connect( $this->$serverName, $this->$connectionInfo);
+
+        if( $this->$connection ) {
+             echo "Connection established.<br />";
+        }
+        else
+        {
+             echo "Connection could not be established.<br />";
+             die(print_r(sqlsrv_errors(), true));
+        }
+    }
 }
+
+$serverName = "REIN\\SQLEXPRESS"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"BibleForAllServerAppv1.Server.Data", "UID"=>"sa", "PWD"=>"password", "CharacterSet" =>"UTF-8");
+
+$dbconnection = new DatabaseConnection($connectionInfo, $serverName);
