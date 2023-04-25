27b652736f065eaf9ecb30cc729f97ac7d2bfb3f
ReinhardBehrens <124669682+ReinhardBehrens@users.noreply.github.com>
2023-04-25 13:57:48 +0200

    Updated link to Zefania Bible Reader
    Updated link to Zefania Bible Reader

--- Footer.php
+++ Footer.php
@@ -17,6 +17,7 @@
                  <div class="col-sm-4">
                      <table>
                         <tr><td><a class="footer-ahref" href="https://github.com/ReinhardBehrens/Bible4All">Download</a></td></tr>
+                        <tr><td><a class="footer-ahref" href="https://sourceforge.net/projects/zefania-sharp/">Download Zefania XML bible reader</a></td></tr>
                      </table>
                  </div>
                  <div class="col-sm-4">
